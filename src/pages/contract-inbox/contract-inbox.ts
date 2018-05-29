bcba7b49d71b9aa5e55a5c8300dea6af0e5dcef4
Lasse Riess <lasse.riess@daimler.com>
2018-05-29 12:11:41 +0200

    Dynamic content bugs fixed

--- src/pages/contract-inbox/contract-inbox.ts
+++ src/pages/contract-inbox/contract-inbox.ts
@@ -18,7 +18,88 @@ import {RestProvider} from "../../providers/rest/rest";
 export class ContractInboxPage {
 
   proposals: any;
-  myUserID= '222';
+  myUserID = '222';
+
+  contracts: any = [{
+    "$class": "org.minsurance.project.Proposal",
+    "contract": "resource:org.minsurance.project.Contract#3172",
+    "contractID": "contract333",
+    "beginDate": "2018-05-29T07:27:07.335Z",
+    "endDate": "2018-05-29T07:27:07.335Z",
+    "description": "fas",
+    "objectID": "object111",
+    "lenderID": "222",
+    "borrowerID": "111",
+    "insuranceID": "i111",
+    "blackBoxID": "black111",
+    "status": "accepted",
+    "price": 500,
+    "insuranceSum": 10,
+    "futureHolder": {
+      "$class": "org.minsurance.project.User",
+      "persoNmb": "2705",
+      "firstName": "dqw",
+      "lastName": "qwd",
+      "email": "dqw",
+      "phoneNmb": "fsa",
+      "birthDate": "asf",
+      "avatarUrl": "fas",
+      "address": {
+        "$class": "org.minsurance.project.Address",
+        "street": "asf",
+        "city": "fas",
+        "zipCode": 0,
+        "country": "Germany"
+      }
+    }
+  }, {
+    "$class": "org.minsurance.project.Proposal",
+    "contract": "resource:org.minsurance.project.Contract#3172",
+    "contractID": "contract333",
+    "beginDate": "2018-05-29T07:27:07.335Z",
+    "endDate": "2018-05-29T07:27:07.335Z",
+    "description": "fas",
+    "objectID": "object111",
+    "lenderID": "222",
+    "borrowerID": "111",
+    "insuranceID": "i111",
+    "blackBoxID": "black111",
+    "status": "opened",
+    "price": 500,
+    "insuranceSum": 10
+  }, {
+    "$class": "org.minsurance.project.Proposal",
+    "contract": "resource:org.minsurance.project.Contract#3172",
+    "contractID": "contract333",
+    "beginDate": "2018-05-29T07:27:07.335Z",
+    "endDate": "2018-05-29T07:27:07.335Z",
+    "description": "fas",
+    "objectID": "object111",
+    "lenderID": "222",
+    "borrowerID": "111",
+    "insuranceID": "i111",
+    "blackBoxID": "black111",
+    "status": "reported",
+    "price": 500,
+    "insuranceSum": 10,
+    "futureHolder": {
+      "$class": "org.minsurance.project.User",
+      "persoNmb": "2705",
+      "firstName": "dqw",
+      "lastName": "qwd",
+      "email": "dqw",
+      "phoneNmb": "fsa",
+      "birthDate": "asf",
+      "avatarUrl": "fas",
+      "address": {
+        "$class": "org.minsurance.project.Address",
+        "street": "asf",
+        "city": "fas",
+        "zipCode": 0,
+        "country": "Germany"
+      }
+    }
+  }];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider) {
   }
@@ -34,11 +115,9 @@ export class ContractInboxPage {
     });
   }
 
-  loadData(){
-    this.restProvider.getProposals()
+  loadData() {
+    this.restProvider.getContracts()
       .then(data => {
-        this.proposals = data;
-        console.log(this.proposals);
 
       });
   }
